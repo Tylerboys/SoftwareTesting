31e4c51fd659485ab3038cb5e4be6855eaba80c0
Li-Steven-pro <58417371+Li-Steven-pro@users.noreply.github.com>
2022-02-09 15:57:25 +0100

    test(employee): improve description of the test

---
+++ employee/new-employee-page.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+import { AddEmployeePage } from '../employee/add-employee.page';
+import { EmployeesPage } from '../employee/employees.page';
+import { Employee } from '../employee/employee';
+
+test.describe('When I am on the add employees page', () => {
+    test('Add new employee with all inputs filled', async ({ page, browserName }) => {
+        test.skip(browserName !== 'firefox', 'Run only on firefox');
+    
+        const employee: Employee = {
+            name: 'Remi',
+            email: 'cce@yahho.com',
+            address1: 'addr1',
+            address2: 'addr1',
+            city: 'paris',
+            zip_code: '744',
+            hiring_date: '4 mars',
+            job_title: 'marchan'
+        }
+    
+        const addEmployeePage = new AddEmployeePage(page);
+        await addEmployeePage.goto();
+        await addEmployeePage.add(employee);
+      
+        const employeesPage = new EmployeesPage(page);
+        await employeesPage.goto();
+    
+        const hasName = await employeesPage.hasName(employee.name);
+        expect(hasName).toBe(true);
+    });
+});
